eb55e469d103ac56693417cf5ae8f1e568734058
KosBelozyorov <KosBelozyorov@gmail.com>
2019-09-10 17:42:21 +0300

    Updates

---
+++ precache-manifest.d9a966a88b201c41186afaead6600c22.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "3b9a01666f00d5e421f48cc71bad014c",
+    "url": "/react_static-list-of-posts/index.html"
+  },
+  {
+    "revision": "8a216afa6710efdef519",
+    "url": "/react_static-list-of-posts/static/css/main.57fe51a6.chunk.css"
+  },
+  {
+    "revision": "893a8acb1a0ad8980363",
+    "url": "/react_static-list-of-posts/static/js/2.f00605ee.chunk.js"
+  },
+  {
+    "revision": "8a216afa6710efdef519",
+    "url": "/react_static-list-of-posts/static/js/main.adb4b67b.chunk.js"
+  },
+  {
+    "revision": "3fdea9cdc8e2a1e9a77e",
+    "url": "/react_static-list-of-posts/static/js/runtime~main.c48bb6e1.js"
+  }
+]);
